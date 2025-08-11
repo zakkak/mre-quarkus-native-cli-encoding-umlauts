8d7ba2657ec20fe8d4fa8a7748be5554539c0df3
Ramona Hartinger <r.hartinger@adito.de>
2025-08-11 13:41:35 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mre-quarkus-native-cli-encoding-umlauts
+
+MRE for trying to log umlauts to the cli in quarkus with native builds.
+
+Building:
+
+````shell
+ cmd /c 'call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" && mvn package -Dnative'
+````
---
+++ src/main/java/org/acme/HelloCommand.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import org.jboss.logging.Logger;
+import picocli.CommandLine;
+
+/**
+ * TODO r.hartinger write comment
+ *
+ * @author r.hartinger, 11.08.2025
+ */
+@CommandLine.Command
+public class HelloCommand implements Runnable {
+
+    private Logger log = Logger.getLogger(HelloCommand.class);
+
+    @Override
+    public void run() {
+        System.out.println("hello with umlauts äöüß");
+        System.out.println("normal line");
+
+        log.warn("hello with umlauts äöüß from logger");
+    }
+}
